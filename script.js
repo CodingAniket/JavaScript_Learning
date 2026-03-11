6d8811761fa450b9ebcb652141bd740687bb1e8a
Aniket Saha <magicgaming741126@gmail.com>
2026-03-11 10:55:05 +0530

    usage of symbol and bigint

---
+++ script.js
@@ -0,0 +1,11 @@
+let a=Number.MAX_SAFE_INTEGER;
+console.log(a+2);
+let x={
+    "id":1,
+    "name":"Aniket",
+    "roll":2570028,
+    "address":"bethuadahari"
+}
+ let u1= Symbol("id");
+x[u1]=001;
+ console.log(x);
