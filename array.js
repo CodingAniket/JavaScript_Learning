02b4156b0439a8c4f53b909e9477aab2365a5b53
Aniket Saha <magicgaming741126@gmail.com>
2026-03-11 15:50:15 +0530

    array readme updation

---
+++ array.js
@@ -0,0 +1,9 @@
+let arr=["Aniket","Saha","Hydrex","DranzR"];
+console.log(arr);
+arr.pop();
+arr.unshift("Mango");
+arr.forEach((val)=>{
+console.log(val);
+})
+let arr2=[55,80,11,64,1,5];
+console.log(arr2.sort((a,b)=>{return a-b}));
